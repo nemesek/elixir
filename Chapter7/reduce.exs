98292f78caf978323ad0cd3944de30ba426b0d2e
Nemesek <dnemesek@gmail.com>
2015-02-23 20:44:27 -0600

    more of chapter 7

--- Chapter7/reduce.exs
+++ Chapter7/reduce.exs
@@ -1,7 +1,8 @@
 defmodule MyList do
   def reduce([], value, _), do: value
   def reduce([head|tail], value, func), do: reduce(tail, func.(head,value), func)
-  def mapsum(list, func), do: reduce(_mapsum(list,func),0,&(&1 + &2))
+  # works def mapsum(list, func), do: reduce(_mapsum(list,func),0,&(&1 + &2))
+  def mapsum(list, func), do: _mapsum(list,func) |> reduce(0, &(&1 + &2))
   defp _mapsum([], _), do: []
   defp _mapsum([head|tail], func), do: [func.(head)| _mapsum(tail,func)]
   def max(list), do: _max(list,0)
