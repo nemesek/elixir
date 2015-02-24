c2849af124497d464d4aa6e2d05549b3d0f28e14
Nemesek <dnemesek@gmail.com>
2015-02-23 21:11:33 -0600

    made mapsum more modular

--- Chapter7/reduce.exs
+++ Chapter7/reduce.exs
@@ -2,9 +2,10 @@ defmodule MyList do
   def reduce([], value, _), do: value
   def reduce([head|tail], value, func), do: reduce(tail, func.(head,value), func)
   # works def mapsum(list, func), do: reduce(_mapsum(list,func),0,&(&1 + &2))
-  def mapsum(list, func), do: _mapsum(list,func) |> reduce(0, &(&1 + &2))
-  defp _mapsum([], _), do: []
-  defp _mapsum([head|tail], func), do: [func.(head)| _mapsum(tail,func)]
+  def mapsum(list, func), do: _map(list,func) |> _sum()
+  defp _map([], _), do: []
+  defp _map([head|tail], func), do: [func.(head)| _map(tail,func)]
+  defp _sum(list), do: reduce(list, 0, &(&1 + &2))
   def max([head|tail]), do: _max(tail,head)
   defp _max([], largest), do: largest
   defp _max([head|tail], largest) when head > largest do _max(tail,head) end
