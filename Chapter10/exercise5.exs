e26b29323700e5c0df4f682ef704c82e0590158b
Nemesek <dnemesek@gmail.com>
2015-02-24 22:21:33 -0600

    implemented Enum split

--- Chapter10/exercise5.exs
+++ Chapter10/exercise5.exs
@@ -12,4 +12,15 @@ defmodule Test do
     func.(head)
     each(tail,func)
   end
+  def split(list, n) do
+     {
+       _take(list,n),
+       _reverse(_take(_reverse(list,[]), length(list) - n),[])
+     }
+   end
+  defp _take([], _)            do [] end
+  defp _take(_, n) when n <= 0 do [] end
+  defp _take([x|xs], n)        do [ x | _take(xs,n-1) ] end
+  defp _reverse([],ys)      do ys end
+  defp _reverse([x|xs], ys) do _reverse(xs, [x|ys]) end
 end
