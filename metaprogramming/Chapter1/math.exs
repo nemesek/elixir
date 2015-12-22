99f6130bf668ec5e8ac1a39aa6c685ca4da01f33
Dan Nemesek <dnemesek@fncinc.com>
2015-12-22 00:48:47 -0600

    better formatting

--- metaprogramming/Chapter1/math.exs
+++ metaprogramming/Chapter1/math.exs
@@ -4,7 +4,7 @@ defmodule Math do
         lhs = unquote(lhs)
         rhs = unquote(rhs)
         result = lhs + rhs
-        IO.puts "#{lhs}plus#{rhs} is #{result}"
+        IO.puts "#{lhs} plus #{rhs} is #{result}"
         result
       end
     end
@@ -14,7 +14,7 @@ defmodule Math do
         lhs = unquote(lhs)
         rhs = unquote(rhs)
         result = lhs * rhs
-        IO.puts "#{lhs}times#{rhs} is #{result}"
+        IO.puts "#{lhs} times #{rhs} is #{result}"
         result
       end
     end
